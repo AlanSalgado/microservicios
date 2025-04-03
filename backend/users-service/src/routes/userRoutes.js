55afe39b873beac259f8e331c566887f3468b4e9
asalas <user.password>
2025-04-03 14:26:14 -0600

    Base del Frontend listo - Integraciónd e bcryptjs y jwt - Integración del login y registro de usuario - Creación del método para hacer login - Ajuste en el método de crear - Integración del middleware - Cambio en el diseño del Frontend - Falta ajustar el middleware en el obtener todos

--- backend/users-service/src/routes/userRoutes.js
+++ backend/users-service/src/routes/userRoutes.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require("../controllers/userController");
+const { getAllUsers, getUserById, createUser, loginUser, updateUser, deleteUser } = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.get("/", getAllUsers);
-router.get("/:id", getUserById);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
 router.post("/register", createUser);
+router.post("/login", loginUser);
+
+router.get("/", getAllUsers);
+router.get("/:id", authMiddleware, getUserById);
+router.put("/:id", authMiddleware, updateUser);
+router.delete("/:id", authMiddleware, deleteUser);
 
 module.exports = router;
